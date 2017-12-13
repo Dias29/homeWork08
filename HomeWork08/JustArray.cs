9a948a3281aeb06043b3e8326e8d155b523c9d0c
Dias29 <33831288+Dias29@users.noreply.github.com>
2017-12-14 01:17:12 +0600

    first

---
+++ HomeWork08/JustArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HomeWork08
+{
+    public class JustArray
+    {
+        private int[] Array;
+        public int this[int index]
+        {
+            get
+            {
+                return Array[index];
+            }
+
+            set
+            {
+                Array[index] = (int)Math.Pow(value, 2);
+            }
+        } 
+
+        public JustArray(int size)
+        {
+            Array = new int[size];
+        }
+    }
+}
---
+++ HomeWork08/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HomeWork08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size;
+            Console.WriteLine("Введите размер массива: ");
+            bool isParsed = int.TryParse(Console.ReadLine(), out size);
+            if (!isParsed)
+            {
+                Console.WriteLine("Ошибка!");
+            }
+            else
+            {
+                JustArray array = new JustArray(size);
+                //Console.WriteLine("\n");
+
+                for (int i = 0; i < size; i++)
+                {
+                    array[i] = i;
+                    Console.WriteLine(i + " = " + array[i]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
